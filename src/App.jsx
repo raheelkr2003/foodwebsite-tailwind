a5428224be5d97df8bfa8f2f62b3733d2ff8ce93
unknown <raheelkr2003@gmail.com>
2024-09-03 09:23:47 +0500

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Navbar/>
+
+    </div>
   )
 }
 
