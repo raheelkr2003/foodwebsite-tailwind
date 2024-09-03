9843feed3922211da1fc31f28921e998a527614d
unknown <raheelkr2003@gmail.com>
2024-09-03 09:18:02 +0500

    install tailwind using vite

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
